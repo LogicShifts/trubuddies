d6384fa4b1cce85517dbc87dacf031b266843079
Somnath Bera <somnathbera1919@gmail.com>
2023-08-18 00:21:20 +0530

    usermodel bug fixes

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -39,6 +39,6 @@ const userSchema = new mongoose.Schema({
 });
 
 //const User = mongoose.model('User', userSchema);
-const User = mongoose.models.User || mongoose.model("User", userSchema);
+const User = mongoose.models.users || mongoose.model("users", userSchema);
 
 export default User;
