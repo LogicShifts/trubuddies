335dd69698ea8b09c14ea87dc025e4fadf6852d7
Somnath Bera <somnathbera1919@gmail.com>
2023-08-17 23:50:54 +0530

    adding register functionality

---
+++ src/models/roleModel.ts
@@ -0,0 +1,13 @@
+// const mongoose = require('mongoose');
+
+// const moodSchema = new mongoose.Schema({
+//   feeling: { type: String, required: true },
+//   whyFeelingThisWay: String,
+//   activities: [String],
+//   createdAt: { type: Date, default: Date.now },
+//   updatedAt: Date
+// });
+
+// const Mood = mongoose.model('Mood', moodSchema);
+
+// module.exports = Mood;
---
+++ src/models/userModel.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: [true, "Please provide a username"],
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: [true, "Please provide an email"],
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: [true, "Please provide a password"],
+	},
+	roles: [{ type: mongoose.Schema.Types.ObjectId, ref: "Role" }],
+	profilePhoto: String,
+	firstName: String,
+	lastName: String,
+	dob: Date,
+	gender: String,
+	phone: String,
+	address: {
+		street: String,
+		city: String,
+		country: String,
+		zipCode: String,
+	},
+	isVerified: { type: Boolean, default: false }, // New field
+	forgotPasswordToken: String, // New field
+	forgotPasswordTokenExpiry: Date, // New field
+	verifyToken: String, // New field
+	verifyTokenExpiry: Date, // New field
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: Date,
+	isActive: { type: Boolean, default: true },
+});
+
+//const User = mongoose.model('User', userSchema);
+const User = mongoose.models.users || mongoose.model("User", userSchema);
+
+export default User;
