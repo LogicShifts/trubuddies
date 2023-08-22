9adaa733be9e9b66dfa01fdaf0926b280cbe5267
Csankar2001 <chandan2001roy@gmail.com>
2023-08-23 00:19:30 +0530

    added articles page and some css changes

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import Hero from "@/section/hero";
+import Footer from "@/components/footer";
+import Header from "@/components/navbar";
+import Article from "@/components/article";
 export default function Articles() {
   return (
-    <>
-      <h1>Articles</h1>
-    </>
+      <main>
+        <Header />
+        <Article />
+        <Footer />
+      </main>
+    
   );
 }
---
+++ src/components/article.tsx
@@ -0,0 +1,31 @@
+export default function Article() {
+    return (
+       <main>
+            <div className="Article-padding">
+                <div className="blogs1">
+                    <h1>Learn & Get Inspired</h1>
+                    <div className="blog1">
+                        <div className="blog-left1"></div>
+                    </div>
+                    <div className="blog1">
+                        <div className="blog-left1"></div>
+                    </div>
+                    <div className="blog1">
+                        <div className="blog-left1"></div>
+                    </div>
+                    <div className="blog1">
+                        <div className="blog-left1"></div>
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <button className="blogbutton1">View more</button>
+                    </div>
+                </div>
+            </div>
+            </main>
+    );
+}
