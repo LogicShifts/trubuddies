51e383e357bce7022ffd240f89b7fd03e60d74b0
Somnath Bera <somnathbera1919@gmail.com>
2023-08-25 17:56:11 +0530

    article page ui fixing

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -3,9 +3,13 @@ import React from 'react'
 const Article = ({title, description}: {title: string, description: string}) => {
     return (
         <div>
-            <div className=" mx-[5%] h-[80px] rounded-[12px] mb-[10px] flex flex-row bg-indigo-200 text-justify">
-                <div className="w-2/5 h-full rounded-l-lg bg-blue-600 text-center justify-center"><p>{title}</p></div>
-                {description}
+            <div className=" mx-[5%] h-[80px] rounded-[12px] mb-[10px] flex flex-row bg-indigo-200 ">
+                <div className="w-2/5 h-full rounded-l-lg bg-blue-600 text-center flex justify-center align-middle text-white" >
+                    <div  className='m-auto'>{title}</div>
+                </div>
+                <div className='w-3/5 text-center' >
+                   <div className='m-auto' >{description}</div>  
+                </div>
             </div>
         </div>
     )
