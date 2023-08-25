d1749b19b7337f4da4b780e736d27df923f1ef20
AYUSH JHA <72190187+A-jha383@users.noreply.github.com>
2023-08-25 22:33:07 +0530

    fix

--- src/app/api/initdb/route.ts
+++ src/app/api/initdb/route.ts
@@ -4,39 +4,31 @@ import User from "@/models/userModel";
 import UserRole from "@/models/userRoleModel";
 import { NextRequest, NextResponse } from "next/server";
 import BuddyProfile from "@/models/buddyProfileModel";
-import TruBuddyProfile from "@/models/truBuddyProfileModel";
+//import TruBuddyProfile from "@/models/truBuddyProfileModel";
 import Mood from "@/models/moodModel";
 import DailyMood from "@/models/diaryModel";
 import ToDo from "@/models/todoModel";
 import TruBuddyReview from "@/models/truBuddyReviewModel";
 
-
-
 connect();
 
-export async function GET(req:NextRequest, res:NextResponse)  {
-  
-      try {
-        await User.find();
-        await BuddyProfile.find();
-        await TruBuddyProfile.find();
-        await UserRole.find();
-        await UserRole.find();
-        await Mood.find();
-        await DailyMood.find();
-        await ToDo.find();
-        await TruBuddyReview.find();
-        
-        return NextResponse.json({
-        message:"init success",
-            success: true,
-        })
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    await User.find();
+    await BuddyProfile.find();
+    await TruBuddyProfile.find();
+    await UserRole.find();
+    await UserRole.find();
+    await Mood.find();
+    await DailyMood.find();
+    await ToDo.find();
+    await TruBuddyReview.find();
 
-       
-
-  }
-  catch (err:any) {
+    return NextResponse.json({
+      message: "init success",
+      success: true,
+    });
+  } catch (err: any) {
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
 }
-
