0808e97fcc87aff5e0cd56d4d8c6cf124969362d
Somnath Bera <somnathbera1919@gmail.com>
2023-08-30 12:56:03 +0530

    trying max listeners fix

--- src/DBconfig/dbConfig.ts
+++ src/DBconfig/dbConfig.ts
@@ -1,19 +1,39 @@
 import mongoose from "mongoose";
 
 
+// export async function connect() {
+//     try{
+//         mongoose.connect(process.env.MONGO_URI!);
+//         const connection = await mongoose.connection;
+//         connection.on('connected', () =>{
+//             console.log('Connected to Mongoose');
+//         });
+//         connection.on('error', (err) =>{
+//             console.log('Error connecting to Mongoose - ' + err.message);
+//             process.exit();
+//         });
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// }
+
 export async function connect() {
-    try{
-        mongoose.connect(process.env.MONGO_URI!);
-        const connection = await mongoose.connection;
-        connection.on('connected', () =>{
+    try {
+        // Check if mongoose instance is in disconnected state
+        if (mongoose.connection.readyState === 0) {
+            await mongoose.connect(process.env.MONGO_URI!);
+        }
+
+        const connection = mongoose.connection;
+        connection.on('connected', () => {
             console.log('Connected to Mongoose');
         });
-        connection.on('error', (err) =>{
+        connection.on('error', (err) => {
             console.log('Error connecting to Mongoose - ' + err.message);
             process.exit();
         });
-    }
-    catch(err){
+    } catch (err) {
         console.log(err);
     }
 }
