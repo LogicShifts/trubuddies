3b3471f6bb11a7b8590f6444e4f02a2ae9eaa706
AYUSH JHA <72190187+A-jha383@users.noreply.github.com>
2023-08-30 07:33:39 +0530

    fixed article page data type

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -10,32 +10,36 @@ import { useEffect, useState } from "react";
 import Article from "@/components/article";
 import FooterSecondary from "@/components/footer/footer2";
 
-
 export default function Articles() {
-  const [articles, setArticles] = useState<{ _id: string; title: string; description: string; }[]>([]);
+  const [articles, setArticles] = useState<
+    {
+      _id: string;
+      title: string;
+      description: string;
+      tags: any;
+      publishedOn: Date;
+      author: string;
+    }[]
+  >([]);
   const [fetchedAll, setFetchedAll] = useState(false);
-  const offset=0;
+  const offset = 0;
   const limit = 20;
-  
-
 
   useEffect(() => {
     async function fetchArticles() {
-      
       try {
-        
         const response = await axios.get("/api/articles", {
           params: {
             limit: limit, // Specify the number of articles to fetch
-            offset:offset
-          }
+            offset: offset,
+          },
         });
         console.log(response.data.data.length);
         setArticles(response.data.data);
-        if(offset+limit > articles.length){
+        if (offset + limit > articles.length) {
           //setDisplayedArticles(displayedArticles + nextArticleLimit + 1);
           setFetchedAll(true);
-      }
+        }
       } catch (error) {
         console.error("Error fetching articles:", error);
       }
@@ -49,8 +53,13 @@ export default function Articles() {
       <Header />
       {/* <ArticleSection/> */}
       <div className="mb-12 mt-2 ">
-        <ArticleSection articles={articles} setArticles={setArticles} fetchedAll={
-        fetchedAll} setFetchedAll={setFetchedAll} limitArticles={limit}/>
+        <ArticleSection
+          articles={articles}
+          setArticles={setArticles}
+          fetchedAll={fetchedAll}
+          setFetchedAll={setFetchedAll}
+          limitArticles={limit}
+        />
       </div>
 
       <FooterSecondary />
