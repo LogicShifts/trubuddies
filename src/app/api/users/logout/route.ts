342337d14035a1053eaff2c82dc0d9c2a7df07df
Somnath Bera <somnathbera1919@gmail.com>
2023-09-01 00:10:44 +0530

    trying to fix token expiry bug

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -7,8 +7,8 @@ export async function GET(request:NextRequest ) {
       success: true,
     });
     //response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
-    console.log(request.nextUrl.hostname)
-    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0), path: '/', domain:  request.nextUrl.hostname });
+    console.log(request.nextUrl.host)
+    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0), path: '/', domain: request.nextUrl.hostname === 'localhost' ? 'localhost': request.nextUrl.host });
     return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
