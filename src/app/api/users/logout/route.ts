4861c7008b0cb5579206f8ce4f677946f8fe7e22
Somnath Bera <somnathbera1919@gmail.com>
2023-09-01 00:02:21 +0530

    trying to fix token expiry bug

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,14 +1,14 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request:NextRequest ) {
   try {
     const response = NextResponse.json({
       message: "Logged out Successfully",
       success: true,
     });
     //response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
-    response.cookies.set("token", "", { httpOnly: true, secure: true, sameSite: 'none', expires: new Date(0) });
-    
+    console.log(request.nextUrl.hostname)
+    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0), path: '/', domain:  request.nextUrl.hostname });
     return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
