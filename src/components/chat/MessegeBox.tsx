25d3e6341817836173fa54f3a86e76a87113b3d8
AYUSH JHA <72190187+A-jha383@users.noreply.github.com>
2023-09-03 22:29:45 +0530

    chat ui fixed

--- src/components/chat/MessegeBox.tsx
+++ src/components/chat/MessegeBox.tsx
@@ -12,7 +12,7 @@ const MessegeBox = () => {
     }
   };
   return (
-    <div className="h-[10%]  p-1 flex flex-row justify-center">
+    <div className="h-[10%] p-1 flex flex-row justify-center">
       <input
         type="text"
         value={message}
@@ -24,7 +24,20 @@ const MessegeBox = () => {
         onClick={handleSendMessage}
         className=" bg-blue-500 text-white rounded-full p-2 m-1 hover:bg-blue-600 focus:outline-none  focus:ring-2 focus:ring-blue-500"
       >
-        ▶{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+          />
+        </svg>
       </button>
     </div>
   );
